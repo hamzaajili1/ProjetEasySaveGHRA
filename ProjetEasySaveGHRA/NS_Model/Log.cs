4e0b4666c46fe4a082940db77ff882eb2a0156cb
Gaby <91410918+gabriel-languille@users.noreply.github.com>
2024-02-09 09:01:54 +0100

    Correction mauvaise branch

--- ProjetEasySaveGHRA/NS_Model/Log.cs
+++ ProjetEasySaveGHRA/NS_Model/Log.cs
@@ -4,32 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
- namespace EasySave.NS_Model
+namespace ProjetEasySaveGHRA.NS_Model
 {
-    class Log
+    internal class Log
     {
-        // --- Attributes ---
-        public string name { get; set; }
-        public string source { get; set; }
-        public string destination { get; set; }
-        public string size { get; set; }
-        public string startTime { get; set; }
-        public string elapsedTime { get; set; }
-
-
-        // --- Constructors ---
-        // Constructor used to Read json File
-        public Log() { }
-
-        // Constructor used to Add a Log
-        public Log(string _name, string _source, string _destination, string _size, string _startTime, string _elapsedTime)
-        {
-            this.name = _name;
-            this.source = _source;
-            this.destination = _destination;
-            this.size = _size;
-            this.startTime = _startTime;
-            this.elapsedTime = _elapsedTime;
-        }
     }
 }
